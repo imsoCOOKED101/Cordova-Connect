c34cca6dfd516d9eb4bad58d9dd1a97c4a60b600
YourName <gamoctobre@gmail.com>
2025-11-23 21:47:59 +0800

    u

--- frontend/app/user/auth/login/page.tsx
+++ frontend/app/user/auth/login/page.tsx
@@ -1,20 +1,26 @@
 import React from 'react'
 
-export default function LoginPage() {
+export default function LoginAdminPage() {
     return(
-        <div className='text'>
-            <div>
+        <div className='text h-screen flex justify-center items-center bg-[#E7F1FF]'>
+            <div className='border p-10 bg-[white] flex flex-col gap-5 text-center shadow-2xl rounded-xl text-gray-500 w-[450px]'>
                 <div>
-                    
+                    <h1 className='text-blue-600 font-bold text-3xl'>Cordova Connect</h1>
+                    <p className='text-sm mt-3'>Sign in your Admin account</p>
                 </div>
                 
-                <form action="">
-                    <label htmlFor=""></label>
-                    <input type="text" />
-                    
-                    <label htmlFor=""></label>
-                    <input type="text" />
-                    <button>Log in</button>
+                <form action="" className='flex flex-col gap-5'>
+                  <div className='flex flex-col text-left gap-3'>
+                    <label htmlFor="" className='font-semibold text-xs'>Student ID</label>
+                    <input type="text" placeholder='20251133' className='border py-3 px-4 rounded-md'/>
+                  </div>
+                  <div className='flex flex-col text-left gap-3'>
+                    <label htmlFor="" className='font-semibold text-xs'>Password</label>
+                    <input type="text" placeholder='*******' className='border py-3 px-4 rounded-md'/>
+                  </div>
+                  <button className='w-full p-3 bg-blue-500 rounded-lg text-white'>Log in</button>
+                  <p className='text-sm'>Dont have and account? <span className='text-blue-500 font-bold ml-2'>Sign up here</span></p>
+
                 </form>
             </div>
         </div>
